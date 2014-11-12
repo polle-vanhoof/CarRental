666ae15a3d3935ff5ed442b6d2ff175fedb22b18
ewoudve <ewoudvanesch@gmail.com>
2014-11-12 15:23:13 +0100

    Reservation annotations

--- CarRental-lib/src/rental/Reservation.java
+++ CarRental-lib/src/rental/Reservation.java
@@ -2,16 +2,10 @@ package rental;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity
 public class Reservation extends Quote implements Serializable{
 
-    @Id @GeneratedValue(strategy= GenerationType.AUTO)
-    private int id;
-    
     private int carId;
     
     /***************
